b2ff791296730619b919581ebdbf7df9473c2c2f
ofekkimchi <ofekkimchi@gmail.com>
2025-04-25 10:27:56 +0300

    end 28.3

---
+++ app/src/main/java/com/example/mylearninggame/Adapters/UserAdapter.java
@@ -0,0 +1,55 @@
+package com.example.mylearninggame.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mylearninggame.Model.User;
+import com.example.mylearninggame.R;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder> {
+    private List<User> userList;
+    private Context context;
+
+    public UserAdapter(Context context, List<User> userList) {
+        this.context = context;
+        this.userList = userList;
+    }
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_user, parent, false);
+        return new UserViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+        User user = userList.get(position);
+        if (user == null) return;
+        holder.userIdTextView.setText(user.getFname());
+    }
+
+    @Override
+    public int getItemCount() {
+        return userList.size();
+    }
+
+    public static class UserViewHolder extends RecyclerView.ViewHolder {
+        TextView userIdTextView;
+        TextView userNameTextView;
+
+        public UserViewHolder(@NonNull View itemView) {
+            super(itemView);
+            userIdTextView = itemView.findViewById(R.id.userIdTextView);
+            userNameTextView = itemView.findViewById(R.id.userNameTextView);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mylearninggame/Screens/UserTable.java
@@ -0,0 +1,36 @@
+package com.example.mylearninggame.Screens;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mylearninggame.R;
+import com.example.mylearninggame.Services.DatabaseService;
+
+public class UserTable extends AppCompatActivity {
+    RecyclerView rv;
+    DatabaseService databaseService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_user_table);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        databaseService= DatabaseService.getInstance();
+
+        rv = findViewById(R.id.rv_users_all);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
